93858dc91777117fa80e8aabb814c99f33564b96
Simonecaste96 <simonecaste96@gmail.com>
2024-03-12 16:00:02 +0100

    commit!!!!!!

--- assets/js/artistiScript.js
+++ assets/js/artistiScript.js
@@ -6,7 +6,7 @@ const container = document.getElementById('container');
 const artista = document.getElementById('nomeartista');
 const ascolti = document.getElementById('ascolti');
 const topArtista = document.getElementById('top-artista');
-const album = document.getElementById('album');
+const albumArtist = document.getElementById('album');
 
 
 window.addEventListener('load', init);
@@ -24,22 +24,24 @@ async function GET() {
     id = urlParametro.get('id');
 
     getArtist()
+    setTimeout(() => {
+        getSongAlbum()
+
+    }, 2000)
 }
 
 async function getArtist() {
-    let artist = await fetch(config.fetchs.artist + id, config.options)
+    let artist = await fetch(config.proxy+config.fetchs.artist + id, config.options)
     artist = await artist.json()
     contenitoreArtist = artist;
     artista.innerText = artist.name;
     ascolti.innerText = artist.nb_fan;
-    console.log('suca1')
     let tracklist = await fetch(config.proxy+artist.tracklist, config.options);
-    console.log('suca2')
     tracklist = await tracklist.json();
     for(i=0; i < 5; i++){
         const li = document.createElement('li')
         li.innerHTML = `
-        <img class="rounded-1"
+        <img class="rounded-1 m-2"
             src="${tracklist.data[i].album.cover_small}" width="3.5%"
             alt="${tracklist.data[i].title_short}" />&nbsp;&nbsp;${tracklist.data[i].title}`
     
@@ -48,17 +50,30 @@ async function getArtist() {
 }
 
 async function getSongAlbum() {
-    let song = await fetch(config.fetch.artist.songs + id, config.options )
-    song = await song.json()
-    contenitoreAlbum = song; 
-    console.log(song)
-    for(i=0; i<10; i++){
-     const li = document.createElement('li');
+    let idMerda = id
+    for(i=0; i < config.artist[id].albums.length; i++){
+        let album = await fetch(config.proxy+config.fetchs.album+config.artist[idMerda].albums[i], config.options);
+        album = await album.json()
+const id = album.id;
+const title =  album.title;
+const cover =  album.cover_medium;
+
+
+     const div = document.createElement('div');
+     div.classList.add('col-2')
     
-     li.innerHTML = ``
+     div.innerHTML = `<a href="album.html?id=${id}">
+     <div class="card bg-card" id="author-${id}">
+         <img src="${cover}" class="card-img-top p-3 m-auto" style="border-radius:20px" alt="${title}">
+         <div class="card-body">
+             <h6 class="card-title">${title}</h6>
+             <p class="card-text text-secondary">Artista</p>
+         </div>
+     </div>
+ </a>`
      
      
 
-     album.appendChild(li);
+     albumArtist.appendChild(div);
     }
-}
+};
