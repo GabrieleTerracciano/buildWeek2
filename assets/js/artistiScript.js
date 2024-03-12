483d09e83e90d8b80c5786da3bc2e0ecd7581341
Simonecaste96 <simonecaste96@gmail.com>
2024-03-12 14:47:13 +0100

    commmmmit

--- assets/js/artistiScript.js
+++ assets/js/artistiScript.js
@@ -1,10 +1,14 @@
 let config = {}
 let id;
-let contenitore = [];
+let contenitoreArtist = [];
+let contenitoreAlbum = [];
 const container = document.getElementById('container');
 const artista = document.getElementById('nomeartista');
 const ascolti = document.getElementById('ascolti');
 const topArtista = document.getElementById('top-artista');
+const album = document.getElementById('album');
+
+
 window.addEventListener('load', init);
 
 function init() {
@@ -25,11 +29,12 @@ async function GET() {
 async function getArtist() {
     let artist = await fetch(config.fetchs.artist + id, config.options)
     artist = await artist.json()
-    console.log(artist.name)
-    contenitore = artist;
+    contenitoreArtist = artist;
     artista.innerText = artist.name;
     ascolti.innerText = artist.nb_fan;
-    let tracklist = await fetch(artist.tracklist, config.options);
+    console.log('suca1')
+    let tracklist = await fetch(config.proxy+artist.tracklist, config.options);
+    console.log('suca2')
     tracklist = await tracklist.json();
     for(i=0; i < 5; i++){
         const li = document.createElement('li')
@@ -43,6 +48,17 @@ async function getArtist() {
 }
 
 async function getSongAlbum() {
-    let song = await fetch(config.fetch.artist.songs + id, config.options)
+    let song = await fetch(config.fetch.artist.songs + id, config.options )
     song = await song.json()
+    contenitoreAlbum = song; 
+    console.log(song)
+    for(i=0; i<10; i++){
+     const li = document.createElement('li');
+    
+     li.innerHTML = ``
+     
+     
+
+     album.appendChild(li);
+    }
 }
